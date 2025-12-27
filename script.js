1e3fefc5b48d2b7ae0bbc2316921be7dd732b96d
shivam Raj <rs959088@gmail.com>
2025-12-28 00:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw74SKoZRs2f2ILZGsIk78f2aO6FmBCmoWikTUmu9MQVl0OwHuHVqwElsT4ZPOYCLLq/exec';
+const form = document.getElementById('contactForm');
+const btn = document.getElementById('submitBtn');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    btn.disabled = true;
+    btn.innerHTML = "Sending...";
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            alert("Success! Your data has been stored.");
+            btn.disabled = false;
+            btn.innerHTML = "Send Message";
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            alert("Error! Please check your internet or script settings.");
+            btn.disabled = false;
+            btn.innerHTML = "Send Message";
+        });
+});
+//////////////////////
+let l = document.querySelector(".fa-youtube");
+function open_github() {
+    window.location.href = "https://github.com/shivam-raj-0";
+}
+function open_linkedin() {
+    window.location.href = "https://www.linkedin.com/in/shivam-raj-619b65329/";
+}
+function open_insta() {
+    window.location.href = "https://www.instagram.com/royal_rajput_shivam_/?hl=en";
+}
+function open_youtube() {
+    window.location.href = "https://www.youtube.com/@royalhindurashtra";
+}
